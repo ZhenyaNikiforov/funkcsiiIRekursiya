415c5089d774a7d058763356e77c00583c6b47df
Zhenya Nikiforov <12A92@mail.ru>
2025-07-24 22:59:30 +0500

    Добавил задание 2 с устранением дублирования.

---
+++ ustranDubl2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    void multiple(int value, int power);
+    
+    int value = 5;
+    int power = 2;
+    
+    multiple(value, power);
+    
+    value = 3;
+    power = 3;
+    
+    multiple(value, power);
+    
+    value = 4;
+    power = 4;
+    
+    multiple(value, power);
+    
+    return 0;
+}
+
+void multiple(int value, int power){
+    
+    int result = 1;
+    
+    for(int i = 0; i < power; i++){
+      result = result * value;
+    }
+    
+    cout<<value<<" в степени "<<power<<" = "<<result<<endl;
+    
+    return;
+}
